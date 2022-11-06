3c7213acb975973b0d62003e816288c76d61d4db
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-11-07 01:34:34 +0545

    update in token verification

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,43 +1,39 @@
-const jwt=  require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const createError =require("./error.js")
+const createError = require("./error.js");
 
-const verifyToken= (req,res,next)=>{
-    const token=req.cookies.access_token
-    if(!token){
-        return next(createError(401,"You are not authenticated!"))
-    } 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"));
+  }
 
-    jwt.verify(token,"secret",(err,user)=>{
+  jwt.verify(token, "secret", (err, user) => {
+    if (err) return next(createError(403, "token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
 
-        if(err) return next(createError(403,"token is not valid!"))
-        req.user =user
-        next()
-    })
+const verifyUser = (req, res, next) => {
+  //  console.log(req.params.userId)
+  verifyToken(req, res, next, () => {
+    if (req.user.id == req.params.userId || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
 
-}
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) { 
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
 
-const verifyUser =(req,res,next) =>{
-    //  console.log(req.params.userId)
-    verifyToken(req,res,next, ()=>{
-        if(req.user.id== req.params.userId || req.user.isAdmin){
-            next()
-        } 
-        else{
-            return next(createError(403, "You are not authorized!"))
-        }
-    })
-}
-
-const verifyAdmin =(req,res,next) =>{
-    verifyToken(req,res,next,()=>{
-        if(req.user.isAdmin){
-            next()
-        }
-        else{
-            return next(createError(403, "You are not authorized!"))
-        }
-    })
-}
-
-module.exports={verifyToken,verifyUser, verifyAdmin}
+module.exports = { verifyToken, verifyUser, verifyAdmin };
