e01f5309e4ac2df8a0e2e2a8a1f8ef7bacc34781
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2022-11-07 17:19:57 +0545

    update in .gitignore

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,7 +1,7 @@
-const jwt=  require("jsonwebtoken")
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const createError =require("./error.js")
+const createError = require("./error.js");
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
@@ -9,36 +9,32 @@ const verifyToken = (req, res, next) => {
     return next(createError(401, "You are not authenticated!"));
   }
 
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return next(createError(403, "token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
 
-        if(err) return next(createError(403,"token is not valid!"))
-        req.user =user
-        next()
-    })
+const verifyUser = (req, res, next) => {
+  //  console.log(req.params.userId)
+  verifyToken(req, res, () => {
+    if (req.user.id == req.query.userId) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
 
-}
-
-const verifyUser =(req,res,next) =>{
-    //  console.log(req.params.userId)
-    verifyToken(req,res,next, ()=>{
-        if(req.user.id== req.query.userId ){
-            next()
-        } 
-        else{
-            return next(createError(403, "You are not authorized!"))
-        }
-    })
-}
-
-const verifyAdmin =(req,res,next) =>{
-    verifyToken(req,res,()=>{
-        if(req.user.isAdmin== req.query.userId){
-            next()
-        }
-        else{
-            return next(createError(403, "You are not authorized!"))
-        }
-    })
-}
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id == req.query.userId) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
+};
 
 module.exports = { verifyToken, verifyUser, verifyAdmin };
